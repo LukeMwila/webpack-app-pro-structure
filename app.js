e5b759cfab56a1f3cad3b2efc489b979ee2a2dcc
Luke Mwila <outlier.developer@gmail.com>
2018-04-03 00:17:55 +0200

    Loop through names in array of people objects.

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const people = require('./people.js')
 const $ = require('jquery')
 
-$('body').append('<h1>'+ people[0].name +'</h1>')
+$.each(people, function(key, value){
+    $('body').append('<h1>'+ people[key].name +'</h1>')
+})
